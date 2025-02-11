df1ab9b2eff8e53e05050a6cda224261c749f269
bananamen4ik <bananamen4ik@icloud.com>
2025-02-11 13:17:08 +0300

    Init tests. Added pytest. Improved building fastapi docker. Fixed error in validation_exception_handler

--- app/exceptions.py
+++ app/exceptions.py
@@ -10,8 +10,8 @@ async def http_exception_handler(_, exc: StarletteHTTPException) -> JSONResponse
         status_code=exc.status_code,
         content={
             "result": False,
-            "error_type": str(type(exc).__name__),
-            "error_message": str(exc.detail)
+            "error_type": type(exc).__name__,
+            "error_message": exc.detail
         }
     )
 
@@ -23,10 +23,7 @@ async def validation_exception_handler(_, exc: RequestValidationError) -> JSONRe
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content={
             "result": False,
-            "error_type": str(type(exc).__name__),
-            "error_message": "{}: {}".format(
-                error["msg"],
-                error["loc"][1]
-            )
+            "error_type": type(exc).__name__,
+            "error_message": error["msg"]
         }
     )
